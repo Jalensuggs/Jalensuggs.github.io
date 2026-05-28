9b95a043bedb96f02c8b1725f5c5b90c71b361bd
Jalensuggs <jalensuggs77@gmail.com>
2026-05-28 19:16:55 +1000

    ci: trigger rebuild with corrected Supabase URL

--- social/src/main.jsx
+++ social/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react' // v2
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
